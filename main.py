3f5f6f05438f36c705650cd2d9d4bb4bdde2b930
asaf halely <asafhalely@gmail.com>
2023-01-14 21:18:25 +0200

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from actions_toolkit.github import Context
 
 context = Context()
 
-PK = os.environ.get("projectKey")
+PK = os.environ.get("project_key")
 # REPO = os.getenv("INPUT_REPO") or os.getenv("GITHUB_REPOSITORY")
 
 # artifacts_url = f"https://api.github.com/repos/{REPO}/actions/artifacts"
